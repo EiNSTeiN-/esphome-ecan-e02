df52b9fd36baec210b53c5f8cd48ace7b04d8ba6
Francois Chagnon <fc@francoischagnon.net>
2026-06-08 23:40:04 -0400

    Remove obsolete git wrapper

--- dev/scripts/git.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-repo_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
-git_dir="$repo_dir/.git-local"
-
-exec git --git-dir="$git_dir" --work-tree="$repo_dir" "$@"
